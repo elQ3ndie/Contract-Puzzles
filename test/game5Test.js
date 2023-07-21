0e423ea6761477675f500b0464c09cc08356e5cc
Andrew Miracle <andrewmiracle300@gmail.com>
2023-07-21 19:13:10 +0100

    Modify contract's tests to ensure it passes

--- test/game5Test.js
+++ test/game5Test.js
@@ -6,14 +6,32 @@ describe('Game5', function () {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    const threshold = '0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf';
+    let wallet = await ethers.Wallet.createRandom().connect(ethers.provider); // Generate a random wallet
+
+    // Keep generating random addresses until one satisfies the condition
+    while (ethers.utils.getAddress(wallet.address) >= ethers.utils.getAddress(threshold)) {
+      wallet = await ethers.Wallet.createRandom().connect(ethers.provider);
+    }
+
+    const address = await wallet.getAddress();
+
+    //Since the random address does not have gas, let's send gas to enable it carry out transactions
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({
+      to: address,
+      value: ethers.utils.parseEther('1')
+    });
+
+    return { game, wallet, address };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, wallet} = await loadFixture(deployContractAndSetVariables);
+
 
     // good luck
-
-    await game.win();
+    const tx = await game.connect(wallet).win();
+    await tx.wait()    
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
